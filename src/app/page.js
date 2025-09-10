b1b13f945e869221307365bfca98244886c0478f
25LP5143 25LP5143 <25LP5143@G305-CC0240.local>
2025-09-10 12:34:50 +0800

    soliv

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,113 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import "./index.css";
+import ShortUniqueId from "short-unique-id";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [state, setState] = useState();
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const handleChangeAllButton = () => {
+    setState("All");
+  };
+
+  const handleChangeAllButton1 = () => {
+    setState("Active");
+  };
+
+  const handleChangeAllButton2 = () => {
+    setState("Completed");
+  };
+  const handleChangeAllButton3 = () => {
+    setState("x");
+  };
+
+  const uid = new ShortUniqueId();
+
+  const handleInputValue = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddButton = () => {
+    const uidWithTimestamp = uid.stamp(32);
+    setTodos([
+      ...todos,
+      { todos: inputValue, status: "Active", id: uidWithTimestamp },
+    ]);
+    setInputValue("");
+  };
+  console.log("this is state value", state);
+  console.log("this is input value", inputValue);
+  console.log("this is todos now", todos);
+
+  return (
+    <div className="container">
+      <h1 className="header">To-Do List</h1>
+      <div className="container1">
+        <input
+          placeholder="Add a new task..."
+          onChange={handleInputValue}
+        ></input>
+        <button className="button1" onClick={handleAddButton}>
+          Add
+        </button>
+      </div>
+      <div className="container2">
+        <button
+          onClick={handleChangeAllButton}
+          style={{
+            backgroundColor: state === "All" ? "#3c82f6" : "#f9f9f9",
+            color: state === "All" ? "#f9f9f9" : "#363636",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          All
+        </button>
+        <button
+          onClick={handleChangeAllButton1}
+          style={{
+            backgroundColor: state === "Active" ? "#3c82f6" : "#f9f9f9",
+            color: state === "Active" ? "#f9f9f9" : "#363636",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Active
+        </button>
+        <button
+          onClick={handleChangeAllButton2}
+          style={{
+            backgroundColor: state === "Completed" ? "#3c82f6" : "#f9f9f9",
+            color: state === "Completed" ? "#f9f9f9" : "#363636",
+          }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Completed
+        </button>
+      </div>
+
+      <h2 className="header1">No tasks yet. Add one above!</h2>
+      <div className="container3">
+        {todos.map((todo) => {
+          return (
+            <div>
+              <button
+                className="button3"
+                onClick={handleChangeAllButton3}
+                style={{
+                  backgroundColor: state === "x" ? "#3c82f6" : "#f9f9f9",
+                  color: state === "x" ? "#f9f9f9" : "white",
+                }}
+              >
+                x
+              </button>
+              {todo.todos}
+              <button className="button4">Delete</button>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h3 className="header2">Powered by</h3>
+        <button className="button2">Pinecone academy</button>
+      </div>
     </div>
   );
 }
